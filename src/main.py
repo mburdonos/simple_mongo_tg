3b3b0270c54ad1397da56c615e2b33a1b133aaab
MAKSIM <mburdonos@mail.ru>
2024-05-20 04:29:36 +0300

    create connection with telegramm bot

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 import asyncio
 import logging
 
+from aiogram import Bot, Dispatcher, types, F
+
 from core.config import settings
 
+
 logging.basicConfig(level=logging.INFO)
+bot = Bot(token=settings.telegram_bot.token)
+dp = Dispatcher()
+
+@dp.message(F.text)
+async def cmd_start(message: types.Message):
+    await message.answer(f'get text: {message.text}')
+async def main():
+    await dp.start_polling(bot)
 
 if __name__=='__main__':
-    print(f'start project: {settings.project.name}')
+    logging.info(f'start project: {settings.project.name}')
+    asyncio.run(main())
